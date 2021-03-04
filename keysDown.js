9dea29f1cc408c105c700641cfde0388cfe93b61
NKY5223 <76890154+NKY5223@users.noreply.github.com>
2021-03-04 08:51:05 +0800

    Fixed e.key being undefined, also added more stuff to oEmbed reeeeee

--- keysDown.js
+++ keysDown.js
@@ -1,8 +1,8 @@
 var keysDown = new Set();
 document.addEventListener("keydown", e => {
     if (e.repeat) return;
-    keysDown.add(e.key.toLowerCase());
+    keysDown.add(e.key?.toLowerCase());
 });
 document.addEventListener("keyup", e => {
-    keysDown.delete(e.key.toLowerCase());
+    keysDown.delete(e.key?.toLowerCase());
 })
