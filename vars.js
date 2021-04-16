5b5a23fc4034329d2ec418bd760352a825796399
NKY5223 <76890154+NKY5223@users.noreply.github.com>
2021-04-17 07:28:42 +0800

    version

--- vars.js
+++ vars.js
@@ -26,7 +26,7 @@ if (URLParams.has("username")) {
 }
 history.replaceState(null, "SkapClient", location.protocol + "//" + location.host + location.pathname);
 
-const version = "yes";
+const version = "removed lotsa names";
 
 /**
  * @type {HTMLCanvasElement}
